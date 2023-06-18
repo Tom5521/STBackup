45ab45e452de72cdef6c570fc2060a87d66b79dd
Tom5521 <88908582+Tom5521@users.noreply.github.com>
2023-06-18 17:49:43 -0400

    modified:   localup.sh

--- localup.sh
+++ localup.sh
@@ -13,64 +13,64 @@ worlds="Backup/public/worlds/"
 backgrounds="Backup/public/backgrounds/"
 back="Backup/"
 groups="Backup/public/groups"
+public="Backup/public"
 
 if [ "$1" == "save" ];then
     echo "Saving Chats"
-    cp -rf public/chats/* $chats
+    cp -rf public/chats/ $public
     echo "Saving Characters"
-    cp -rf public/characters/* $characters
+    cp -rf public/characters/ $public
     echo "Saving OpenAI Settings"
-    cp -rf public/"OpenAI Settings"/* Backup/public/'OpenAI Settings'
+    cp -rf public/"OpenAI Settings"/ $public
     echo "Saving Themes"
-    cp -rf public/themes/* $themes
+    cp -rf public/themes/ $public
     echo "Saving Worlds"
-    cp -rf public/worlds/* $worlds
+    cp -rf public/worlds/ $public
     echo "Saving User Avatars"
-    cp -rf public/'User Avatars'/* Backup/public/'User Avatars'
+    cp -rf public/'User Avatars'/ $public
     echo "Saving Backgrounds"
-    cp -rf public/backgrounds/* $backgrounds
+    cp -rf public/backgrounds/ $public
     echo "Saving Group Chats"
-    cp -rf public/'group chats'/* Backup/public/'group chats'
+    cp -rf public/'group chats'/ $public
     echo "Saving Groups"
-    cp -rf public/groups/* $group
+    cp -rf public/groups/ $public
     echo "Saving Thumbnails"
     cp -rf thumbnails $back
     echo 'Saving "secrets.json"'
     cp -rf secrets.json $back
     echo "Saving Configs"
     cp -rf config.conf $back
-    cp -rf secrets.json $back
-    cp -rf public/settings.json Backup/public/
-    cp -rf public/i18n.json Backup/public/
+    cp -rf public/settings.json $public
+    cp -rf public/i18n.json $public
     
 fi
 
 if [ "$1" == "restore" ]; then
     echo "Restoring Chats"
-    cp -rf $chats* public/chats/
+    cp -rf $chats public/
     echo "Restoring Characters"
-    cp -rf $characters* public/characters/
+    cp -rf $characters public/
     echo "Restoring OpenAI settings"
-    cp -rf Backup/public/'OpenAI Settings'/* public/"OpenAI Settings"/
+    cp -rf Backup/public/'OpenAI Settings'/ public/
     echo "Restoring Themes"
-    cp -rf $themes* public/themes/
+    cp -rf $themes public/
     echo "Restoring Worlds"
-    cp -rf $worlds* public/worlds/
+    cp -rf $worlds public/
     echo "Restoring User Avatars"
-    cp -rf Backup/public/'User Avatars'/* public/'User Avatars'/
+    cp -rf Backup/public/'User Avatars'/ public/
     echo "Restoring Backgrounds"
-    cp -rf $backgrounds* public/backgrounds/
+    cp -rf $backgrounds public/
     echo "Restoring Group Chats"
-    cp -rf Backup/public/'group chats'/* public/'group chats'/
+    cp -rf Backup/public/'group chats'/ public/
     echo "Restoring groups"
-    cp -rf $groups* public/groups/
+    cp -rf $groups public/
     echo "Restoring Thumbnails"
-    cp -rf thumbnails $back
+    cp -rf Backup/thumbnails .
     echo 'Restoring "secrets.json"'
-    cp -rf secrets.json $back
+    cp -rf Backup/secrets.json .
     echo "Restoring Configs"
     cp -rf Backup/config.conf .
-    cp -rf Backup/settings.json .
+    cp -rf Backup/settings.json public/
     cp -rf Backup/public/i18n.json public/
 fi
 
