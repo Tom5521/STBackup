28e82cca330629c601260fd68e632fc0c019ae52
Tom5521 <88908582+Tom5521@users.noreply.github.com>
2023-06-22 12:50:29 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cd builds
 
 mkdir x86-64
 cd x86-64
+export GOOS=linux
 export GOARCH=amd64
 go build -o backup-x86-64 ../../backup.go
 
@@ -14,6 +15,7 @@ cd ..
 
 mkdir aarch64
 cd aarch64
+export GOOS=android
 export GOARCH=arm64
 go build -o backup-aarch64 ../../backup.go
 
