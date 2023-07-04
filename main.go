5f043347fbdce4cc14178f0d658e67be3d4837d2
Tom5521 <mismarttv321@gmail.com>
2023-07-04 16:13:57 -0400

    Delete annoying and inefficient variables Rename variables to improve readability

--- main.go
+++ main.go
@@ -18,49 +18,49 @@ var root string = getdata.Root
 
 // MAIN
 func main() {
-	log.Loginfo("--------Start--------")
+	log.Info("--------Start--------")
 	rebuildcheck := update.RebuildCheck()
-	defer log.Loginfo("---------End---------")
+	defer log.Info("---------End---------")
 	os.Chdir(root)
 	if tools.CheckBranch() == false {
-		log.Logwarn("You are in the dev branch!")
+		log.Warning("You are in the dev branch!")
 		fmt.Println("Note: You are using the dev branch. Which is usually always broken and is more for backup and anticipating changes than for users to experiment with.Please go back to the main branch, which is functional.")
 	}
 	if len(os.Args) < 2 && !rebuildcheck {
-		log.Logerror("Option not specified.")
+		log.Error("Option not specified.")
 		fmt.Println("Option not specified...")
 		return
 	}
 	switch os.Args[1] {
 	case "make":
-		log.Logfunc("Make")
+		log.Func("Make")
 		os.Chdir("..")
 		os.MkdirAll("Backup/public", os.ModePerm)
 	case "save":
-		log.Logfunc("save")
+		log.Func("save")
 		os.Chdir("..")
 		tools.Cmd("rsync -av --progress " + getdata.Exclude_Folders + "--delete . " + " " + back)
 		os.Chdir(root)
-		log.Loginfo("Files Saved")
+		log.Info("Files Saved")
 		if len(os.Args) == 3 {
 			if os.Args[2] == "tar" {
-				log.Logfunc("save tarball")
+				log.Func("save tarball")
 				os.Chdir("..")
 				tar := tools.Cmd("tar -cvf Backup.tar Backup/")
 				if tar != 0 {
-					log.Loginfo("Tarbal created.")
+					log.Info("Tarbal created.")
 				}
 			}
 		}
 	case "restore":
-		log.Logfunc("restore")
+		log.Func("restore")
 		os.Chdir("..")
 		if len(os.Args) == 3 {
 			if os.Args[2] == "tar" {
 				ls, _ := tools.ReadCommand("ls")
-				log.Logfunc("restore from tarball")
+				log.Func("restore from tarball")
 				if strings.Contains(ls, "Backup") {
-					log.Logwarn("Removing Backup/ folder")
+					log.Warning("Removing Backup/ folder")
 					tools.Cmd("rm -rf Backup/")
 				}
 				tools.Cmd("tar -xvf Backup.tar")
@@ -68,38 +68,38 @@ func main() {
 		}
 		tools.Cmd("rsync -av --progress " + getdata.Exclude_Folders + getdata.Include_Folders + "--delete " + back + " . ")
 		os.Chdir(root)
-		log.Loginfo("Files restored")
+		log.Info("Files restored")
 	case "route":
 		if len(os.Args) < 3 {
 			fmt.Println("Backup destination not specified")
-			log.Logerror("Not enough arguments")
+			log.Error("Not enough arguments")
 			return
 		}
 		os.Chdir("..")
 		tools.Cmd("mv Backup/ " + os.Args[2] + " -f")
 		os.Chdir(root)
-		log.Logfunc("route")
+		log.Func("route")
 		if os.Args[3] == "tar" {
-			log.Logfunc("route tar")
+			log.Func("route tar")
 			os.Chdir("..")
 			tools.Cmd("mv Backup.tar " + os.Args[2] + " -f")
-			log.Loginfo("Tar file moved to" + os.Args[2])
+			log.Info("Tar file moved to" + os.Args[2])
 		}
 	case "start":
-		log.Logfunc("start")
+		log.Func("start")
 		tools.Cmd("node ../server.js")
-		log.Loginfo("SillyTavern ended")
+		log.Info("SillyTavern ended")
 	case "update":
 		if len(os.Args) < 2 {
 			fmt.Println("Nothing Selected")
-			log.Logerror("Nothing selected in update func")
+			log.Error("Nothing selected in update func")
 			return
 		}
 		if os.Args[2] == "ST" {
 			os.Chdir("..")
 			tools.Cmd("git pull")
 			os.Chdir(root)
-			log.Loginfo("SillyTavern Updated")
+			log.Info("SillyTavern Updated")
 		}
 		if os.Args[2] == "me" {
 			_, ggit := tools.ReadCommand("git status")
@@ -108,26 +108,26 @@ func main() {
 			if !strings.Contains(err, "main.go") || err2 == 1 || ggit == 1 {
 				if err2 == 1 {
 					fmt.Println("No go compiler found... Downloading binaries")
-					log.Logerror("No go compiler found. Downloading binaries")
+					log.Error("No go compiler found. Downloading binaries")
 				}
 				bindata, _ := tools.ReadCommand("file backup")
 				if strings.Contains(bindata, "x86-64") {
-					log.Loginfo("Downloading x86-64 binary")
+					log.Info("Downloading x86-64 binary")
 					update.UpdateBin("pc")
 				}
 				if strings.Contains(bindata, "ARM aarch64") {
-					log.Loginfo("Downloading aarch64 binary")
+					log.Info("Downloading aarch64 binary")
 					update.UpdateBin("Termux")
 				}
 			} else {
 				tools.Cmd("git pull")
-				log.Loginfo("Updated with git")
+				log.Info("Updated with git")
 				update.Rebuild()
 			}
 			tools.Cmd("./backup link")
 		}
 	case "ls":
-		log.Logfunc("ls")
+		log.Func("ls")
 		tools.Cmd("rclone ls " + getdata.Remote)
 	case "upload":
 		tools.Rclone("up")
@@ -144,10 +144,10 @@ func main() {
 			}
 		}
 	case "init":
-		log.Logfunc("init")
+		log.Func("init")
 		tools.Cmd("bash ../start.sh")
 	case "link":
-		log.Logfunc("link")
+		log.Func("link")
 		os.Chdir("..")
 		file, _ := os.Create("backup")
 		defer file.Close()
@@ -156,11 +156,11 @@ func main() {
 		cont += "./backup $1 $2 $3 $4\n"
 		file.WriteString(cont)
 		os.Chmod("backup", 0700)
-		log.Loginfo("linked")
+		log.Info("linked")
 	case "version":
 		fmt.Println("SillyTavernBackup version", getdata.Version, "\nUnder the MIT licence\nCreated by Tom5521")
 	case "remote":
-		log.Logfunc("remote")
+		log.Func("remote")
 		tools.Makeconf()
 	case "cleanlog":
 		tools.Cmd("echo '' > app.log")
@@ -172,7 +172,7 @@ func main() {
 	case "help":
 		fmt.Println("Please read the documentation in https://github.com/Tom5521/SillyTavernBackup\nAll it's in the README")
 	default:
-		log.Logerror("Option not specified.")
+		log.Error("Option not specified.")
 		fmt.Println("Option not specified...")
 	}
 }
